d1ac59d7d734de68e260749754248c743d86c652
vouchheang <vouchheang.mom@institute.pse.ngo>
2024-06-06 09:24:31 +0700

    dom1

--- dom1/index.js
+++ dom1/index.js
@@ -2,3 +2,9 @@ const article = {
   title: "How to code",
   description: "You can learn to code. Error is nothing to be afraid of.",
 };
+function myFunction() {
+  document.getElementById("article").innerHTML += `
+  <div>${article.title}</div>
+  <div>${article.description}</div>
+  `}
+  
